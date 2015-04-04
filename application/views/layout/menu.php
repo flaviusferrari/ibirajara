9c7263e35875e39f4687d98b06f41d6535f4d6c8
flavius <flavius@flavius-System-Product-Name>
2015-04-04 10:46:08 -0300

    Ajustes no Menu do Painel

--- application/views/layout/menu.php
+++ application/views/layout/menu.php
@@ -8,7 +8,7 @@
             <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> 
                 <span class="sr-only">Toggle navigation</span> 
             </button> 
-            <a class="navbar-brand" href="<?php echo base_url('principal') ?>">Home</a>
+            <a class="navbar-brand" href="index" target="new">CEI</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
@@ -18,10 +18,11 @@
                 <li class="dropdown">
                     <a class="dropdown-toggle" href="#" data-toggle="dropdown">Cadastros<b class="caret"></b></a>
                     <ul class="dropdown-menu">
-                        <li><a href="<?php echo base_url('clientespf') ?>">Clientes</a></li>
+                        <li><a href="<?php echo base_url('indexCode.php/administrator/programacao'); ?>">Programação</a></li>
+                        <li><a href="<?php echo base_url('indexCode.php/administrator/boletim'); ?>">Boletim</a></li>
                     </ul>
                 </li>
-                <li><a href="<?php echo base_url('login/logout') ?>">Sair</a></li>
+                <li><a href="<?php echo base_url('indexCode.php/login/logout') ?>">Sair</a></li>
             </ul>            
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container -->
