7bdba77674985b56b1986dae1166e408d75f277d
flavius <flavius@flavius-System-Product-Name>
2015-06-21 09:28:42 -0300

    Inclusão do Submenu no template CodeIgniter

--- application/views/conteudo/midia/videos.php
+++ application/views/conteudo/midia/videos.php
@@ -1,1 +1,10 @@
-videos
+
+<!-- CONTENT -->
+<div class="panel panel-primary">
+    <!-- Panel heading -->
+    <div class="panel-heading">Vídeos</div>
+    <!-- Panel Body -->
+    <div class="panel-body">
+        
+    </div> <!-- /panel-body -->
+</div> <!-- / panel-primary -->
---
+++ application/views/conteudo/sidebar/sidebar_site.php
@@ -0,0 +1,45 @@
+
+    <!-- Sub-menu -->
+    <div class="panel panel-primary">                        
+        <!-- Panel heading -->
+        <div class="panel-heading">Menu</div>                        
+        <!-- Panel Body -->
+        <div class="panel-body">
+            <!-- Slide Show -->
+            <?php include_once 'app/include/sub.phtml'; ?>
+        </div> <!-- /panel-body -->                        
+    </div> <!-- / panel-primary -->
+
+    <!-- VÍDEOS -->
+    <div class="panel panel-primary">
+        <!-- Panel Heading -->
+        <div class="panel-heading">Últimos Vídeos</div>
+        <!-- Panel Body -->
+        <div class="panel-body">
+            <iframe style="width: 100%;" src="https://www.youtube.com/embed/axTd_DmsNNw?showinfo=0" frameborder="0" allowfullscreen></iframe>
+        </div>
+    </div>
+
+    <!-- LIVROS -->
+    <div class="panel panel-primary">                        
+        <!-- Panel heading -->
+        <div class="panel-heading">Livro da Semana</div>                        
+        <!-- Panel Body -->
+        <div class="panel-body">
+            <img src="template/images/Nosso_lar_min.jpg">
+            <p>Nosso Lar </p>
+        </div> <!-- /panel-body -->                        
+    </div> <!-- / panel-primary -->
+
+    <!-- AVISOS -->
+    <div class="panel panel-primary">                        
+        <!-- Panel heading -->
+        <div class="panel-heading">Avisos</div>                        
+        <!-- Panel Body -->
+        <div class="panel-body">
+            Captamos doações de: roupas, ítens de higiene, destinados a moradores em situação de rua;
+            absorventes, leite em pó e gêneros, fralda de pano, escova e pente para bebês (as roupas devem
+            estar limpas e em boas condições de uso), e luvas de procedimento M e G e fralda geriátrica
+            M e G para AC Vicente Moretti.
+        </div> <!-- /panel-body -->                        
+    </div> <!-- / panel-primary -->
