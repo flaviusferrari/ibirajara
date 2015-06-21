b9181f3fefe6ccda8d191e0886408d6b19a6d96c
flavius <flavius@flavius-System-Product-Name>
2015-06-21 13:17:25 -0300

    Página de vídeos

--- application/views/conteudo/midia/videos.php
+++ application/views/conteudo/midia/videos.php
@@ -6,5 +6,33 @@
     <!-- Panel Body -->
     <div class="panel-body">
         
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <!-- Vídeo principal -->
+                <div class="embed-responsive embed-responsive-4by3">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/axTd_DmsNNw?showinfo=0" frameborder="0" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+        
+        <hr>
+        
+        <div class="row">
+            <div class="col-md-4">
+                <iframe style="width: 90%;" src="https://www.youtube.com/embed/yYmiOJKdpj0?showinfo=0" frameborder="0" allowfullscreen></iframe>
+            </div>
+            
+            <div class="col-md-4">
+                <iframe style="width: 90%;" src="https://www.youtube.com/embed/dQ-mlfk7_TA?showinfo=0" frameborder="0" allowfullscreen></iframe>
+            </div>
+            
+            <div class="col-md-4">
+                <iframe style="width: 90%;" src="https://www.youtube.com/embed/i_6gpN3smAQ?showinfo=0" frameborder="0" allowfullscreen></iframe>
+            </div>
+        </div>
+        
+        
+        
+    
     </div> <!-- /panel-body -->
 </div> <!-- / panel-primary -->
