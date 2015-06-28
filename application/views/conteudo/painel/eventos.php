0bc2f1368ccf2e54e337b5a627c90866e4cd606a
flavius <flavius@flavius-System-Product-Name>
2015-06-28 19:12:16 -0300

    Início do formulário de Cadastro de Eventos

--- application/views/conteudo/painel/eventos.php
+++ application/views/conteudo/painel/eventos.php
@@ -1,7 +1,34 @@
-<!-- TÍTULO DA SESSÃO -->
-<div class="panel panel-default sessao">
+<div class="panel panel-primary">
+    <!-- Panel heading -->
+    <div class="panel-heading">Cadastro de Eventos</div>
+    <!-- Panel Body -->
     <div class="panel-body">
-        <span>Cadastro de Eventos</span>
+        
+        <!-- BOTÕES -->
+        <div id="menuBotoes">
+            <a class="btn btn-primary" id="btnSalvar">Salvar</a>
+            <a class="btn btn-primary" id="btnNovo">Novo</a>
+            <a class="btn btn-primary" id="btnAtualizar">Atualizar</a>
+            <a class="btn btn-primary" id="btnLocalizar">Localizar</a>
+            <a class="btn btn-primary" id="btnExcluir">Excluir</a>                    
+        </div>    
+        
+        <br>
+        
+        <form id="formulario" method="POST" autocomplete="off" action="">
+            
+            <!-- DATA INICIO -->                
+            <div class="row">
+                <div class="col-md-2 form-group">
+                    <label>Data</label>
+                    <input name="data" id="data" class="form-control dtForm" value="<?php echo set_value('data'); ?>">  
+                    <?php echo form_error('data', '<div class="label label-danger">', '</div>'); ?>
+                    <input type="hidden" name="idEvento" id="idEvento" value="">
+                </div>  
+            </div>   
+            
+        </form>
+        
     </div>
-</div>
+</div><!-- /panel-primary -->
 
