bb8b7cb525ec1d2851bc8b5fdafeae08462ab267
flavius <flavius@flavius-System-Product-Name>
2015-07-05 11:20:30 -0300

    Página Eventos pelo CodeIgniter

---
+++ application/controllers/Eventos.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Eventos extends CI_Controller
+{
+    // DADOS
+    private $dados;
+    
+    /**
+        * Método __construct()
+        *  método construtor da classe
+        */
+    public function __construct()
+    {
+        parent::__construct();
+        
+        // DADOS
+        $this->dados['css'] = '';
+        $this->dados['js']  = '';
+        $this->dados['conteudo'] = 'site/eventos_view';
+        
+    }
+
+    public function index()
+    {
+        
+        $this->load->view('site', $this->dados);
+    }
+    
+}
