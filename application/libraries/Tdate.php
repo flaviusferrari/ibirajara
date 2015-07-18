59e053d1951803561e26afa6e4a902b5edf9401f
flavius <flavius@flavius-System-Product-Name>
2015-07-18 17:35:57 -0300

    Biblioteca Tdate retornando o nome do mês por extenso

--- application/libraries/Tdate.php
+++ application/libraries/Tdate.php
@@ -24,4 +24,57 @@ class Tdate
         return $data;
     }
     
+    
+    /**
+     * Método getNomeMes()
+     *   retorna o nome do Mês por extenso
+     * 
+     * @param $mes = valor do Mês
+     */
+    public function getNomeMes($mes)
+    {
+        // Verifica qual o mes corrente
+        switch ($mes)
+        {
+            case 01:
+                $mes = "JANEIRO";
+                break;
+            case 02:
+                $mes = 'FEVEREIRO';
+                break;
+            case 03:
+                $mes = 'MARÇO';
+                break;
+            case 04:
+                $mes = 'ABRIL';
+                break;
+            case 05:
+                $mes = 'MAIO';
+                break;
+            case 06:
+                $mes = 'JUNHO';
+                break;
+            case 07:
+                $mes = 'JULHO';
+                break;
+            case 08:
+                $mes = 'AGOSTO';
+                break;
+            case 09:
+                $mes = 'SETEMBRO';
+                break;
+            case 10:
+                $mes = 'OUTUBRO';
+                break;
+            case 11:
+                $mes = 'NOVEMBRO';
+                break;
+            case 12:
+                $mes = 'DEZEMBRO';
+                break;
+        }
+        
+        return $mes;
+    }
+    
 }
