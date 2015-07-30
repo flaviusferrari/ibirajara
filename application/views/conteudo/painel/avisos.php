8ccb1cd3dcae8427ebbed3c152f90a05881f1df8
flavius <flavius@flavius-System-Product-Name>
2015-07-30 17:10:42 -0300

    Página de Cadastro de Avisos

---
+++ application/views/conteudo/painel/avisos.php
@@ -0,0 +1,10 @@
+<div class="panel panel-primary">
+    <!-- Panel heading -->
+    <div class="panel-heading">Cadastro de Avisos</div>
+    <!-- Panel Body -->
+    <div class="panel-body">
+        
+        
+        
+    </div>
+</div><!-- /panel-primary -->
