2394a7a3dab53896f7e8be3ea2e83f4ab46485db
flavius <flavius@flavius-System-Product-Name>
2015-08-10 14:42:08 -0300

    Atualização dos vídeos

--- application/views/conteudo/midia/videos.php
+++ application/views/conteudo/midia/videos.php
@@ -7,14 +7,14 @@
     <div class="panel-body">
         
         <blockquote>
-            <p class="lead">Animismo - Porque o medo?</p>
+            <p class="lead">A vida é uma só... - Augusto Haanwinkel</p>
         </blockquote>
         
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <!-- Vídeo principal -->
                 <div class="embed-responsive embed-responsive-4by3">
-                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/YqFnkgZlEMo?showinfo=0" frameborder="0" frameborder="0" allowfullscreen></iframe>
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/1RjJC99E9A8?showinfo=0" frameborder="0" frameborder="0" allowfullscreen></iframe>
                 </div>
             </div>
         </div>
@@ -22,11 +22,23 @@
         <hr>
         
         <div class="row">
+            <div class="col-md-4">                
+                <iframe style="width: 90%;" src="https://www.youtube.com/embed/a47VyV3kR_k?showinfo=0" frameborder="0" allowfullscreen></iframe>
+                <p class="lead">75 Anos de retorno de Ibirajara ao Plano Espiritual - Deusa Nogueira</p>
+            </div>
+            
+            <div class="col-md-4">                
+                <iframe style="width: 90%;" src="https://www.youtube.com/embed/YqFnkgZlEMo?showinfo=0" frameborder="0" allowfullscreen></iframe>
+                <p class="lead">Animismo: Porque o medo? - Angela Delou </p>
+            </div>
+            
             <div class="col-md-4">
                 <iframe style="width: 90%;" src="https://www.youtube.com/embed/_tEf3sKEY1Y?showinfo=0" frameborder="0" allowfullscreen></iframe>
                 <p class="lead">67 anos do Grupo de Estudos Asdrubal</p>
             </div>
-            
+        </div>
+        
+        <div class="row">            
             <div class="col-md-4">
                 <iframe style="width: 90%;" src="https://www.youtube.com/embed/axTd_DmsNNw?showinfo=0" frameborder="0" frameborder="0" allowfullscreen></iframe>
                 <p class="lead">Seminário Cláudio e Íris Sinotti - Parte 1</p>
@@ -36,21 +48,21 @@
                 <iframe style="width: 90%;" src="https://www.youtube.com/embed/yYmiOJKdpj0?showinfo=0" frameborder="0" allowfullscreen></iframe>
                 <p class="lead">Seminário Cláudio e Íris Sinotti - Parte 2</p>
             </div>
-        </div>
-        
-        <div class="row">
-            
             
             <div class="col-md-4">
                 <iframe style="width: 90%;" src="https://www.youtube.com/embed/dQ-mlfk7_TA?showinfo=0" frameborder="0" allowfullscreen></iframe>
                 <p class="lead">30 anos de ESDE no CEI</p>
             </div>
-            
+        </div>
+        
+        <div class="row">
             <div class="col-md-4">
                 <iframe style="width: 90%;" src="https://www.youtube.com/embed/i_6gpN3smAQ?showinfo=0" frameborder="0" allowfullscreen></iframe>
                 <p class="lead">Saúde e Espiritismo</p>
             </div>
         </div>
+        
+        
         
         
         
