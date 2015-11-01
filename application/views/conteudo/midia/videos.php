f1e54edfe6f46ef1e7798770841e7e06a2c94cd2
flavius <flavius@flavius-System-Product-Name>
2015-11-01 20:23:02 -0200

    Vídeos: Inclusão de novos vídeos

--- application/views/conteudo/midia/videos.php
+++ application/views/conteudo/midia/videos.php
@@ -7,14 +7,14 @@
     <div class="panel-body">
         
         <blockquote>
-            <p class="lead">Colóquio: Solidão - Deyse Lourenço e Claudia Amaral</p>
+            <p class="lead">Seminário Aprendendo com os Relacionamentos - Dayse Lourenço e Claudia Amaral</p>
         </blockquote>
         
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <!-- Vídeo principal -->
                 <div class="embed-responsive embed-responsive-4by3">
-                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/XpyTqV8d8ws?showinfo=0" frameborder="0" allowfullscreen></iframe>
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/sVk0XIrIT1o?showinfo=0" frameborder="0" allowfullscreen></iframe>
                 </div>
             </div>
         </div>
@@ -23,6 +23,28 @@
         
         <div class="row">
             <div class="col-md-4">                
+                <iframe style="width: 90%;" src="https://www.youtube.com/embed/5MeKz7qUubk?showinfo=0" frameborder="0" allowfullscreen></iframe>
+                <p class="lead">Alienação x Evangelização - Alfredo Moreira </p>
+            </div>
+            
+            <div class="col-md-4">                
+                <iframe style="width: 90%;" src="https://www.youtube.com/embed/yRtxmCggmJ8?showinfo=0" frameborder="0" allowfullscreen></iframe>
+                <p class="lead">Evangelização é compromisso dos Pais? - Iára Telles - C.E.João Batista</p>
+            </div>
+            
+            <div class="col-md-4">                
+                <iframe style="width: 90%;" src="https://www.youtube.com/embed/OQn0HiyoB0U?showinfo=0" frameborder="0" allowfullscreen></iframe>
+                <p class="lead">A Infância - Telma Cerqueira (SEJ)</p>
+            </div>            
+        </div>
+        
+        <div class="row">
+            <div class="col-md-4">                
+                <iframe style="width: 90%;" src="https://www.youtube.com/embed/XpyTqV8d8ws?showinfo=0" frameborder="0" allowfullscreen></iframe>
+                <p class="lead">Colóquio: Solidão - Deyse Lourenço e Claudia Amaral</p>
+            </div>
+            
+            <div class="col-md-4">                
                 <iframe style="width: 90%;" src="https://www.youtube.com/embed/mSRitb4WJ4M?showinfo=0" frameborder="0" allowfullscreen></iframe>
                 <p class="lead">Campanha pela Vida: Vem! Segue-me! Disse Jesus... - Marcelo Nazaré</p>
             </div>
@@ -30,15 +52,15 @@
             <div class="col-md-4">                
                 <iframe style="width: 90%;" src="https://www.youtube.com/embed/gcGoEBrqAzc?showinfo=0" frameborder="0" allowfullscreen></iframe>
                 <p class="lead">75 Anos do C.E.I - Esperanças e Consolações - Marilucia Duarte</p>
-            </div>
-            
+            </div>            
+        </div>
+        
+        <div class="row">
             <div class="col-md-4">                
                 <iframe style="width: 90%;" src="https://www.youtube.com/embed/88hmWTQCVTs?showinfo=0" frameborder="0" allowfullscreen></iframe>
                 <p class="lead">75 Anos do C.E.I - O Que Acontece para Além dos Estudos? - Denise Duarte </p>
             </div>
-        </div>
-        
-        <div class="row">
+            
             <div class="col-md-4">                
                 <iframe style="width: 90%;" src="https://www.youtube.com/embed/AMH0LaPIIF4?showinfo=0" frameborder="0" allowfullscreen></iframe>
                 <p class="lead">75 Anos do C.E.I - Prelúdio do Retorno... - Ricardo Bedirian</p>
