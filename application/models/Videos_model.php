451a611e96b35811fd7a7477f16c54396ed4e995
flavius <flavius@flavius-System-Product-Name>
2016-02-14 10:20:51 -0200

    Vídeos: Criação do Método getVideos no model

--- application/models/Videos_model.php
+++ application/models/Videos_model.php
@@ -4,7 +4,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Videos_model extends CI_Model
 {
     
-    
+    /**
+     *  MÉTODO GET VIDEOS
+     * 
+     *    Busca todos os vídeos salvos no Banco de Dados
+     */
+    public function getVideos()
+    {
+        $this->db->order_by('dtVideo', 'DESC');
+        return $this->db->get('videos');
+    }
     
     
 }
