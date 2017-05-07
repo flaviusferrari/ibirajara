4dc9babe26d3a32495267a492b93778c34e93983
flavius <flavius@flavius-System-Product-Name>
2017-05-07 16:09:57 -0300

    Painel: Container do Sidebar

--- application/views/conteudo/sidebar/boletim.php
+++ application/views/conteudo/sidebar/boletim.php
@@ -1,1 +1,8 @@
-esta é uma sidebar...
+
+<div class="panel panel-primary">
+    <div class="panel-heading" id="tituloSidebar"></div>
+    
+    <div class="panel-body" id="conteudoSidebar">
+        
+    </div>
+</div>
