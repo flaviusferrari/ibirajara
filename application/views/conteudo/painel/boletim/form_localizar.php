6df4f9652c8d8e60d32e0d7d350a15c8c6c834a9
flavius <flavius@flavius-System-Product-Name>
2017-05-07 16:15:45 -0300

    Painel: Ajustado o Layout da localização de Boletins

--- application/views/conteudo/painel/boletim/form_localizar.php
+++ application/views/conteudo/painel/boletim/form_localizar.php
@@ -1,18 +1,19 @@
-
-<div class="container-fluid padding" id="formContainer">
-        
+<div class="panel panel-primary">
+    <div class="panel-heading">Localizar Boletins</div>
+    
+    <div class="panel-body">
         <!-- FORMULÁRIO LOCALIZAR -->
         <form id="formulario" name="formulario" method="post" action=""> 
             
             <div class="row">
-                <!-- DATA INICIO -->
+                <!-- Data Início -->
                 <div class="col-md-2 form-group">
                     <label>Data Inicio:</label>
                     <input name="dtInicio" id="dtInicio" class="form-control dtForm" value="<?php echo set_value('dtInicio'); ?>">  
                     <?php echo form_error('dtInicio', '<div class="label label-danger">', '</div>'); ?>
                     <input type="hidden" name="idBoletim" id="idBoletim" value="">
                 </div>  
-                <!-- DATA FIM -->
+                <!-- Data Fim -->
                 <div class="col-md-2 col-md-offset-1 form-group">
                     <label>Data Fim:</label>
                     <input name="dtFim" id="dtFim" class="form-control dtForm" value="<?php echo set_value('dtFim'); ?>">  
@@ -37,7 +38,6 @@
 
         </form> 
     </div>
-
-    <hr>
+</div>
     
-    <div class="padding" id="listagem"></div>
+<div class="padding" id="listagem"></div>
