c2e85d2c118b14b992e52ab01cc54e3c840b6537
flaviusferrari <flaviusferrari@gmail.com>
2017-11-15 16:00:56 -0200

    Mudado o title da tela de Login do Painel

--- application/views/v_login.php
+++ application/views/v_login.php
@@ -4,8 +4,8 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>FFC Sistemas</title>
-        <base href="/vigapetro/">
+        <title>Ibirajara - Painel de Controle</title>
+        <base href="/ibirajara/">
 
         <!-- CSS -->        
         <link href="<?php echo base_url('includes/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet" type="text/css" />
