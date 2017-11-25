613b554b13d02e19520bf7b70cdb682189dee1bc
flaviusferrari <flaviusferrari@gmail.com>
2017-11-25 10:53:27 -0200

    Configurando o e-mail para enviar via Localhost

---
+++ application/config/email.php
@@ -0,0 +1,12 @@
+<?php
+
+// Configurando o e-mail para enviar via LOCALHOST
+$config['protocol']  = 'smtp';
+$config['smtp_host'] = 'smtp.gmail.com';
+$config['smtp_user'] = 'flaviusferrari@gmail.com';
+$config['smtp_pass'] = 'f24f01c75';
+$config['smtp_port'] = 587;
+$config['charset']   = 'utf-8';
+$config['newline']   = "\r\n";
+$config['smtp_crypto'] = 'tls';
+$config['mailtype'] = 'html'; 
