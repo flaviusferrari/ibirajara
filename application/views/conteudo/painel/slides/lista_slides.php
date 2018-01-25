0f6d858740909b1366dfa2562e527b964112e426
flaviusferrari <flaviusferrari@gmail.com>
2018-01-25 10:54:42 -0200

    Painel de controle: Inserido o título do Slide na listagem

--- application/views/conteudo/painel/slides/lista_slides.php
+++ application/views/conteudo/painel/slides/lista_slides.php
@@ -1,10 +1,12 @@
 <table class="table table-hover"> 
     <thead>
+        <th>Título</th>
         <th>Data Início</th>
         <th>Data Fim</th>
     </thead>
     <?php foreach ($slides as $campo): ?>
     <tr>                   
+        <td><?php echo $campo['titulo']; ?></td>
         <td><?php echo $this->tdate->setDateBr($campo['dtInicio']); ?></td>
         <td><?php echo $this->tdate->setDateBr($campo['dtFinal']); ?></td>        
     </tr>
