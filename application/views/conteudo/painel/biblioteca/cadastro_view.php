25c4ad1f5ad6913063ea99e6f93fd0a260df8b09
flaviusferrari <flaviusferrari@gmail.com>
2018-10-25 15:16:07 -0200

    Biblioteca: Formulário de Cadastro

--- application/views/conteudo/painel/biblioteca/cadastro_view.php
+++ application/views/conteudo/painel/biblioteca/cadastro_view.php
@@ -1,1 +1,62 @@
-Esse é o cadastro da biblioteca.
+
+<div class="panel panel-primary">
+    <div class="panel-heading">
+        Cadastro de Livros
+    </div>
+
+    <div class="panel-body"> 
+        <!-- BOTÕES -->
+        <div class="row">
+            <div id="menuBotoes" class="col-md-12 form-group">
+                <a class="btn btn-primary" id="btnSalvar">Salvar</a>
+                <a class="btn btn-primary" id="btnNovo">Novo</a>
+                <a class="btn btn-primary disabled" id="btnAtualizar">Atualizar</a>
+                <a class="btn btn-primary" id="btnLocalizar">Localizar</a>
+                <a class="btn btn-primary disabled" id="btnExcluir">Excluir</a>                    
+            </div>     
+        </div>
+
+        <form id="formulario" method="POST" autocomplete="off" action="">
+
+            <!-- TÍTULO -->
+            <div class="form-group">
+                <label>Título:</label>
+                <input name="titulo" id="titulo" class="form-control" value="<?php echo set_value('titulo'); ?>">
+                <?php echo form_error('titulo', '<div class="label label-danger">', '</div>'); ?>
+            </div> 
+            
+            <div class="row">
+                <!-- Autor -->
+                <div class="col-md-6 form-group">
+                    <label>Autor:</label>
+                    <input name="dtInicio" id="dtInicio" class="form-control" value="<?php echo set_value('dtInicio'); ?>">  
+                    <?php echo form_error('dtInicio', '<div class="label label-danger">', '</div>'); ?>
+                </div>  
+                <!-- Espírito -->
+                <div class="col-md-6 form-group">
+                    <label>Espírito:</label>
+                    <input name="dtFim" id="dtFim" class="form-control" value="<?php echo set_value('dtFim'); ?>">  
+                    <?php echo form_error('dtFim', '<div class="label label-danger">', '</div>'); ?>
+                </div>                
+            </div>    
+
+            <div class="row">
+                <!-- Editora -->
+                <div class="col-md-6 form-group">
+                    <label>Editora:</label>
+                    <input name="citacao" class="form-control" value="<?php echo set_value('citacao'); ?>" type="text">
+                    <?php echo form_error('citacao', '<div class="label label-danger">', '</div>'); ?>
+                </div>
+            </div>
+
+            <div class="row">
+                <!-- RESENHA -->
+                <div class="col-md-12 form-group">
+                    <label>Resenha:</label>
+                    <textarea name="texto" class="form-control" rows="15"><?php echo set_value('texto'); ?></textarea>
+                    <?php echo form_error('texto', '<div class="label label-danger">', '</div>'); ?>
+                </div>  
+            </div>
+        </form>
+    </div>
+</div>
